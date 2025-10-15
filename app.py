b4d1ec4b78d77dde90e861813e92fc1fac41b6e1
aTrujillo04 <trujillo.antonio.3c@gmail.com>
2025-10-15 10:08:23 -0600

    service.py

--- app.py
+++ app.py
@@ -1,32 +1,6 @@
 import flet as ft
 import requests
-
-RASP_IP = "http://192.168.1.169:5000"
-
-def controlar_dispositivo_gpio(dispositivo, estado):
-    accion = "on" if estado == "encender" else "off"
-    try:
-        respuesta = requests.post(
-            f"{RASP_IP}/control",
-            json={"dispositivo": dispositivo, "accion": accion},
-            timeout=3
-        )
-        data = respuesta.json()
-        print(f"✅ Enviado: {dispositivo} -> {accion} | Respuesta: {data}")
-    except requests.exceptions.RequestException as e:
-        print(f"⚠️ Error al conectar con Raspberry ({dispositivo}): {e}")
-
-def set_motor_pwm(valor):
-    try:
-        respuesta = requests.post(
-            f"{RASP_IP}/pwm",
-            json={"valor": int(valor)},
-            timeout=3
-        )
-        data = respuesta.json()
-        print(f"✅ PWM motor: {valor}% | Respuesta: {data}")
-    except requests.exceptions.RequestException as ex:
-        print(f"⚠️ Error al enviar PWM: {ex}")
+from service import controlar_dispositivo_gpio, set_motor_pwm 
 
 def main(page: ft.Page):
     page.title = "Panel de Control Octynomo"
